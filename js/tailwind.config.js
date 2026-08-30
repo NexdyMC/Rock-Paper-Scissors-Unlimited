d7f4c91877482e7e782434ef70953eaafad95221
NexdyMC <febripratama1786@gmail.com>
2026-08-30 21:43:21 +0700

    update a code

---
+++ js/tailwind.config.js
@@ -0,0 +1,17 @@
+  tailwind.config = {
+    darkMode: 'class',
+    theme: {
+      extend: {
+        fontFamily: {
+          display: ['Anton', 'sans-serif'],
+          body: ['Inter', 'sans-serif'],
+          mono: ['JetBrains Mono', 'monospace'],
+        },
+        colors: {
+          corner1: '#ff3b5c', // merah sudut "Juara Bertahan"
+          corner2: '#3ba7ff', // biru sudut "Penantang"
+          gold:    '#ffd166', // aksen badge VS & pemenang
+        },
+      }
+    }
+  }
